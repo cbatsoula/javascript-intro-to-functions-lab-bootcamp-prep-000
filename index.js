f7462911e6a6e516a85ebd496d0163217fb37ca2
Catherine Batsoula <cbatsoula@gmail.com>
2019-01-22 00:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function sayHiToGrandma(string) {
   var mixedCase = 'Hi there'
   var sayHiToGrandma = (string)
   
-}  if ((sayHiToGrandma = string.toLowerCase)) {
+}  if ((string = string.toLowerCase)) {
     return  "I can't hear you!";
 
 
-}  else if ((sayHiToGrandma = string.toUpperCase)) {
+}  else if ((string = string.toUpperCase)) {
      return "YES INDEED!";
 
- }  else if ((sayHiToGrandma = string.toMixedCase)) {
+ }  else if ((string = string.toMixedCase)) {
     return "I love you, too.";
 }
