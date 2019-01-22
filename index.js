b1bab6e469a085db081160c0db9cde80b0e33317
Catherine Batsoula <cbatsoula@gmail.com>
2019-01-22 00:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
   
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log('string')
+  console.log('string');
 }
-
 function logWhisper(string) {
-  console.log('string')
+  console.log('string');
 }
 function sayHiToGrandma(string) {
-  var lowerCase = 'hello'
-  var upperCase = 'HELLO'
-  var mixedCase = 'Hi there'
-  var sayHiToGrandma = (string)
+  var lowerCase = 'hello';
+  var upperCase = 'HELLO';
+  var mixedCase = 'Hi there';
+  var sayHiToGrandma = (string);
   
 }  if ((string = string.toLowerCase)) {
     return  "I can't hear you!";
