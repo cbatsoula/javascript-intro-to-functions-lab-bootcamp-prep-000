2de011f2b89f402f901b6b43ba9c2c764ebf8c4f
Catherine Batsoula <cbatsoula@gmail.com>
2019-01-22 01:00:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,10 @@ function sayHiToGrandma(string) {
   var upperCase = 'HELLO';
   var mixedCase = 'Hi there';
   var sayHiToGrandma = (string);
-  
 }  if ((string = string.toLowerCase)) {
   return"I can't hear you!";
-
-
 }  else if ((string = string.toUpperCase)) {
     return"YES INDEED!";
-
  }  else if ((string = string.toMixedCase)) {
   return"I love you, too.";
 }
